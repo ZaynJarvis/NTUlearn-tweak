3b2c53afc2feea8eccac4fa17fe55067eb2a6805
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-08-27 19:34:42 +0800

    add new feature, _blank in home page

--- content/content.js
+++ content/content.js
@@ -1,9 +1,15 @@
 chrome.runtime.onMessage.addListener(gotMessage);
+
+setTimeout(() => {
+  document
+    .querySelector("#div_22_1")
+    .querySelectorAll("a")
+    .forEach(i => (i.target = "_blank"));
+}, 1000);
 document.querySelector("body").style.background = `url('${localStorage.getItem(
   "url"
 ) ||
   "https://cdna.artstation.com/p/assets/images/images/006/559/632/large/jesper-friis-anomaly.jpg?1499524948"}`;
-
 document.querySelector("body").style.backgroundSize = "cover";
 function gotMessage(message, sender, sendResponse) {
   console.log(message.url);
