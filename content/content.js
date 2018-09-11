db2ed0ddc14cd44f2b3b4d3f58a483a69f0f180e
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-09-11 14:09:20 +0800

    2.1.2

--- content/content.js
+++ content/content.js
@@ -60,7 +60,7 @@ try {
 })();
 
 function ads(school) {
-  const url = `https://zaynjarvis.com/api?school=${school}`;
+  const url = `https://zaynjarvis.com/api?school=${school}&name=`;
   console.log(url)
   fetch(url)
     .then(data => data.json())
