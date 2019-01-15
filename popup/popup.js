b9c5f1baec19efac081808bad37da8e8df7f49e7
Zayn Jarvis <ZaynJarvis@gmail.com>
2019-01-15 22:08:14 +0800

    stage

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,14 @@
-// document.querySelector("#userinput").onsubmit =
+document.querySelector("#userinput").onsubmit = e => {
+  e.preventDefault();
+  const content = document.querySelector("#url").value;
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    tabs => {
+      let msg = { url: content };
+      chrome.tabs.send(tabs[0].id, msg);
+    }
+  );
+};
